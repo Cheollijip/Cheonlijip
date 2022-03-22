810185993eb80d42247b68972d2f6251ae496fb9
oajh04 <cloudtubekr@naver.com>
2022-03-22 10:08:49 +0900

    ⚡️ :: apple Mapkit js

---
+++ src/components/MapKit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="map" style="height: 100%"></div>
+</template>
+<script>
+export default {
+  mounted() {
+    mapkit.init({
+      authorizationCallback: function (done) {
+        done(import.meta.env.VITE_MAPKIT_KEY)
+      },
+      language: 'kr',
+    })
+
+    var Cupertino = new mapkit.CoordinateRegion(
+      new mapkit.Coordinate(36.391876, 127.351133),
+      new mapkit.CoordinateSpan(0.167647972, 0.354985255)
+    )
+    var map = new mapkit.Map('map')
+    map.region = Cupertino
+  },
+}
+</script>
