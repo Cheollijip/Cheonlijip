0fe36308c1d0e7a7693dfd4eedc4a3e47e7fa079
oajh04 <cloudtubekr@naver.com>
2022-03-23 20:20:39 +0900

    ⚡️ :: 지도

--- src/components/MapKit.vue
+++ src/components/MapKit.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="map" style="height: 100%"></div>
+  <img src="../assets/location.svg" />
+  <matjip-tap />
 </template>
 <script>
+import MatjipTap from './MatjipTap.vue'
 export default {
+  components: { MatjipTap },
+  data() {
+    return {
+      Cupertino: null,
+    }
+  },
   mounted() {
     mapkit.init({
       authorizationCallback: function (done) {
@@ -11,12 +20,35 @@ export default {
       language: 'kr',
     })
 
-    var Cupertino = new mapkit.CoordinateRegion(
+    var MarkerAnnotation = mapkit.MarkerAnnotation
+    var myMarker = new mapkit.Coordinate(36.391876, 127.351133)
+    var myAnnotation = new MarkerAnnotation(myMarker, {
+      color: '#34C557',
+      title: '천리집',
+    })
+
+    this.Cupertino = new mapkit.CoordinateRegion(
       new mapkit.Coordinate(36.391876, 127.351133),
       new mapkit.CoordinateSpan(0.167647972, 0.354985255)
     )
     var map = new mapkit.Map('map')
-    map.region = Cupertino
+
+    map.addEventListener('region-change-end', function () {
+      console.log(map.center)
+    })
+
+    map.showItems([myAnnotation])
+    map.region = this.Cupertino
   },
 }
 </script>
+
+<style>
+img {
+  position: absolute;
+  z-index: 2;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
