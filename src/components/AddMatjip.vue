3210b98bbd864e1e0f1ba353392079ae6d757fdb
oajh04 <cloudtubekr@naver.com>
2022-04-05 08:56:01 +0900

    ⚡️ :: 맛집 추가 컴포넌트

---
+++ src/components/AddMatjip.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="background">
+    <div id="map" style="height: 100%"></div>
+    <img src="../assets/location.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Cupertino: null,
+    }
+  },
+  mounted() {
+    mapkit.init({
+      authorizationCallback: function (done) {
+        done(import.meta.env.VITE_MAPKIT_KEY)
+      },
+      language: 'kr',
+    })
+
+    this.Cupertino = new mapkit.CoordinateRegion(
+      new mapkit.Coordinate(36.391876, 127.351133),
+      new mapkit.CoordinateSpan(0.167647972, 0.354985255)
+    )
+    var map = new mapkit.Map('map')
+
+    map.addEventListener('region-change-end', function () {
+      console.log(map.center)
+    })
+
+    map.region = this.Cupertino
+  },
+}
+</script>
+
+<style>
+.background {
+  width: 100%;
+  height: 100vh;
+  touch-action: none;
+  overflow: none;
+}
+
+img {
+  position: absolute;
+  z-index: 2;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
