dd0ee4b1756b71e4fb5cb1a85a94074a9ee7ec9f
oajh04 <cloudtubekr@naver.com>
2022-04-05 08:56:19 +0900

    ⚡️ :: 맛집 추가 view

---
+++ src/views/MatjipCreate.vue
@@ -0,0 +1,15 @@
+<template>
+  <add-matjip></add-matjip>
+</template>
+
+<script>
+import AddMatjip from '../components/AddMatjip.vue'
+
+export default {
+  components: {
+    AddMatjip,
+  },
+}
+</script>
+
+<style scoped></style>
