8665de44f6f0edce75872e85cd5816e7d1b2d877
oajh04 <cloudtubekr@naver.com>
2022-04-04 13:44:49 +0900

    ⚡️ :: index

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MapKit from '../components/MapKit.vue'
+import Landing from '../views/Landing.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', component: MapKit }],
+  routes: [
+    { path: '/', component: MapKit },
+    { path: '/landing', component: Landing },
+  ],
 })
 export default router
