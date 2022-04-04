ac31c0c88975a9925ff6bb07a7c0dbe6caa64aa3
oajh04 <cloudtubekr@naver.com>
2022-04-05 08:56:24 +0900

    ⚡️ :: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Main from '../views/Main.vue'
 import Landing from '../views/Landing.vue'
+import MatjipCreate from '../views/MatjipCreate.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Main },
     { path: '/landing', component: Landing },
+    { path: '/create', component: MatjipCreate },
   ],
 })
 export default router
