98fcb5d8284f4f4192598a1c042d901a62680f49
oajh04 <cloudtubekr@naver.com>
2022-04-04 14:21:23 +0900

    ⚡️ :: Main

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <Mapkit />
+  <matjip-tap></matjip-tap>
+</template>
+
+<script>
+import Mapkit from '../components/MapKit.vue'
+import MatjipTap from '../components/MatjipTap.vue'
+
+export default {
+  components: { Mapkit, MatjipTap },
+}
+</script>
+
+<style scoped></style>
