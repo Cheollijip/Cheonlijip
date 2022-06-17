630cf7b830652a53427d5776d8d3511bc39ad872
oajh04 <cloudtubekr@naver.com>
2022-06-18 01:39:35 +0900

    ⚡️ :: 타이틀 제거

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="wrapper">
-    <div class="title">
-      <div>천리집</div>
-      <div>우리 학교 주변 맛집</div>
-    </div>
     <div class="Mock-up">
       <transition name="left-image" appear>
         <img src="../assets/LeftIOS.png" class="left" />
@@ -35,17 +31,6 @@
   flex-direction: column;
 }
 
-.title {
-  padding: 16px;
-}
-.title div:nth-child(1) {
-  font-size: 34px;
-  font-weight: bold;
-}
-.title div:nth-child(2) {
-  font-size: 17px;
-}
-
 .Mock-up {
   display: flex;
   justify-content: center;
@@ -56,12 +41,14 @@ img {
   width: 170px;
   height: 369px;
 }
+
 .left {
   position: relative;
   top: 130px;
   left: 35px;
   animation: bound 4s infinite;
 }
+
 .right {
   position: relative;
   top: 200px;
