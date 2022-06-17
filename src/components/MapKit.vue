ca30c80e5e86c404ddf590d77e1242d88f37fc84
oajh04 <cloudtubekr@naver.com>
2022-06-18 03:29:13 +0900

    ⚡️ :: Mapkit js

--- src/components/MapKit.vue
+++ src/components/MapKit.vue
@@ -5,35 +5,86 @@
 </template>
 
 <script>
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+
 export default {
   data() {
     return {
       Cupertino: null,
+      dataa: {},
     }
   },
+  computed: {
+    ...mapGetters(['getMatjipList']),
+  },
+  methods: {
+    ...mapMutations(['CHOICE_MATJIP']),
+    ...mapActions(['fetchMatjipList']),
+
+    mapFetch() {
+      mapkit.init({
+        authorizationCallback: function (done) {
+          done(import.meta.env.VITE_MAPKIT_KEY)
+        },
+        language: 'kr',
+      })
+
+      const choiceDataFetch = (data) => {
+        this.CHOICE_MATJIP(data)
+        console.log('asdsdadasd')
+      }
+
+      var calloutDelegate = {
+        calloutElementForAnnotation: function ({ landmark }) {
+          choiceDataFetch(landmark)
+
+          var element = document.createElement('div')
+          element.className = 'review-callout'
+          element.textContent = landmark.name
+
+          element.style.padding = '5px'
+          element.style.borderRadius = '10px'
+          element.style.backgroundColor = 'white'
+          return element
+        },
+      }
+
+      const data = this.getMatjipList.map((i) => {
+        return {
+          coordinate: new mapkit.Coordinate(i.latitude, i.longitude),
+          ...i,
+        }
+      })
+
+      var annotations = data.map((landmark) => {
+        var annotation = new mapkit.MarkerAnnotation(landmark.coordinate, {
+          color: '#34C557',
+          callout: calloutDelegate,
+          glyphImage: {
+            1: 'https://walkhub.s3.ap-northeast-2.amazonaws.com/walkhub/c60addb5-6149-4873-a9aa-62827bdbb1d2lotation.png',
+          },
+        })
+        annotation.landmark = landmark
+        return annotation
+      })
+
+      this.Cupertino = new mapkit.CoordinateRegion(
+        new mapkit.Coordinate(36.39196, 127.351198),
+        new mapkit.CoordinateSpan(0.015, 0.015)
+      )
+      var map = new mapkit.Map('map')
+
+      map.showItems(annotations)
+      map.region = this.Cupertino
+    },
+  },
+  created() {
+    this.fetchMatjipList()
+  },
   mounted() {
-    mapkit.init({
-      authorizationCallback: function (done) {
-        done(import.meta.env.VITE_MAPKIT_KEY)
-      },
-      language: 'kr',
-    })
-
-    var MarkerAnnotation = mapkit.MarkerAnnotation
-    var myMarker = new mapkit.Coordinate(36.391876, 127.351133)
-    var myAnnotation = new MarkerAnnotation(myMarker, {
-      color: '#34C557',
-      title: '천리집',
-    })
-
-    this.Cupertino = new mapkit.CoordinateRegion(
-      new mapkit.Coordinate(36.391876, 127.351133),
-      new mapkit.CoordinateSpan(0.167647972, 0.354985255)
-    )
-    var map = new mapkit.Map('map')
-
-    map.showItems([myAnnotation])
-    map.region = this.Cupertino
+    setTimeout(() => {
+      this.mapFetch()
+    }, 1000)
   },
 }
 </script>
