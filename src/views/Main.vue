4de12c142115968077eed5d909169e943555e80d
oajh04 <cloudtubekr@naver.com>
2022-06-19 00:35:28 +0900

    ⚡️ :: API call

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,11 +4,18 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 import Mapkit from '../components/MapKit.vue'
 import MatjipTap from '../components/MatjipTap.vue'
 
 export default {
   components: { Mapkit, MatjipTap },
+  methods: {
+    ...mapActions(['fetchMatjipList']),
+  },
+  created() {
+    this.fetchMatjipList(this.$route.params.token)
+  },
 }
 </script>
 
