0361543e3dc52e722262e0db7ddc728ff80c08e9
oajh04 <cloudtubekr@naver.com>
2022-06-19 23:15:59 +0900

    ⚡️ :: main

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,17 @@
 <template>
   <Mapkit />
   <matjip-tap></matjip-tap>
+  <score-input-modal></score-input-modal>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import Mapkit from '../components/MapKit.vue'
 import MatjipTap from '../components/MatjipTap.vue'
+import ScoreInputModal from '../components/ScoreInputModal.vue'
 
 export default {
-  components: { Mapkit, MatjipTap },
+  components: { Mapkit, MatjipTap, ScoreInputModal },
   methods: {
     ...mapActions(['fetchMatjipList']),
   },
